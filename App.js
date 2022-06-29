67afa0af40088b90027a1cdd87d01e33eb43668f
Daniela3121 <53196225+Daniela3121@users.noreply.github.com>
2022-06-29 15:05:47 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,22 @@
+import './index.css';
+import{Route} from 'react-router-dom';
+import AllMeetupsPage from './pages/AllMeetups';
+import NewMeetupsPage from './pages/NewMeetups';
+import FavoritesPage from './pages/Favorites';
+function App() {
+  return (
+    <div>
+      <Route path='/'>
+        <AllMeetupsPage/>
+      </Route>
+      <Route path='/new-meetups'>
+        <NewMeetupsPage/>
+      </Route>
+      <Route path='/favorites'>
+        <FavoritesPage/>
+      </Route>
+    </div>
+  );
+}
+
+export default App;
